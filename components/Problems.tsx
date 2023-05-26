5a7504e9e871bd7a2376aa6ad99a555d2ab2d3b8
nazimboudeffa <boudeffa@gmail.com>
2023-05-26 19:53:57 +0200

    Fixing localstorage

--- components/Problems.tsx
+++ components/Problems.tsx
@@ -22,16 +22,12 @@ const Problems = () => {
 		<>
 			<tbody>
 				{problems.map((problem : Problem, idx : number) => {
-					let solved = null;
-					if (typeof window !== 'undefined') {
-						// Perform localStorage action
-						solved = localStorage.getItem(`solved-${problem.slug}`);
-					};
-					const difficulyColor = problem.difficulty === "Easy" ? "text-green-400" : problem.difficulty === "Medium" ? "text-yellow-400" : "text-pink-400";
+					const solved = localStorage.getItem(`solved-${problem.slug}`);
+					const difficulyColor = problem.difficulty === "Easy" ? "text-green-400" : problem.difficulty === "Medium" ? "text-yellow-400" : "text-red-400";
 					return (
 						<tr className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`} key={problem.id}>
 							<th className='px-2 py-4 font-medium whitespace-nowrap text-green-400'>
-								{solved === "true" ? <BsCheckCircle fontSize={"18"} width='18' /> : <BsCircle fontSize={"18"} width='18' />}
+								{solved === "true" ? (<BsCheckCircle fontSize={"18"} width='18' />) : (<BsCircle fontSize={"18"} width='18' />)}
 							</th>
 							<td className='px-6 py-4'>
                                 <Link
