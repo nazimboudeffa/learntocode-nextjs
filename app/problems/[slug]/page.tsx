a4616d19d2d05fac88a1f1b0558f8e8dd20ae040
nazimboudeffa <boudeffa@gmail.com>
2023-05-26 15:29:48 +0200

    Fixing static routes

--- app/problems/[slug]/page.tsx
+++ app/problems/[slug]/page.tsx
@@ -4,9 +4,8 @@ import Workspace from "@/components/Workspace/Workspace";
 import useHasMounted from "@/hooks/useHasMounted";
 import { Problem } from "@/utils/types/problem";
 import { problems } from "@/utils/problems";
-import { useState } from "react";
 
-const ProblemPage: React.FC = (context) => {
+const ProblemPage: React.FC = (context : any) => {
 	const hasMounted = useHasMounted();
 	if (!hasMounted) return null;
 	const getData = (slug: string) => {
--- components/Workspace/Workspace.tsx
+++ components/Workspace/Workspace.tsx
@@ -4,19 +4,26 @@ import Split from "react-split"
 import ProblemDescription from "./ProblemDescription/ProblemDescription"
 import Playground from "./Playground/Playground"
 import { Problem } from "@/utils/types/problem"
+import useWindowSize from "@/hooks/useWindowSize"
+import { useState } from "react"
+import Confetti from "react-confetti";
 
 type WorkspaceProps = {
 	problem: Problem
 };
 
 const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+	const { width, height } = useWindowSize();
+	const [success, setSuccess] = useState(false);
+	const [solved, setSolved] = useState(false);
 	return (
-		<Split className='split'>
+		<Split className='split'  minSize={0}>
 			<div> 
-				<ProblemDescription problem={problem} />
+				<ProblemDescription problem={problem} _solved={solved}/>
 			</div>
 			<div>
-				<Playground problem={problem} />
+				<Playground problem={problem} setSuccess={setSuccess} setSolved={setSolved} />
+				{success && <Confetti gravity={0.3} tweenDuration={4000} width={width - 1} height={height - 1} />}
 			</div>
 		</Split>
 	)
