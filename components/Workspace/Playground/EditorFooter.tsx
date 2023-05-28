f82a8741384bc48393822d90452a8c7fddf48a6e
nazimboudeffa <boudeffa@gmail.com>
2023-05-28 17:22:25 +0200

    Moved Console into its component

--- components/Workspace/Playground/EditorFooter.tsx
+++ components/Workspace/Playground/EditorFooter.tsx
@@ -9,14 +9,7 @@ const EditorFooter: React.FC<EditorFooterProps> = ({ handleSubmit }) => {
 	return (
 		<div className='flex bg-dark-layer-1 absolute bottom-0 z-10 w-full'>
 			<div className='mx-5 my-[10px] flex justify-between w-full'>
-				<div className='mr-2 flex flex-1 flex-nowrap items-center space-x-4'>
-					<button className='px-3 py-1.5 font-medium items-center transition-all inline-flex bg-dark-fill-3 text-sm rounded-lg pl-3 pr-2'>
-						Console
-						<div className='ml-1 transform transition flex items-center'>
-							<BsChevronUp className='fill-gray-6 mx-1 fill-dark-gray-6' />
-						</div>
-					</button>
-				</div>
+				{/* here goes the console */}
 				<div className='ml-auto flex items-center space-x-4'>
 					<button
 						className='px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex rounded-lg bg-green-400'
